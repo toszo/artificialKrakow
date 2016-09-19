fc9f2c897fffcbf8237e16e26a02263150f45a48
Tomasz Szostak <tszostak@b-mind.pl>
2016-09-19 12:10:15 +0200

    cartpole refactored

--- src/Q.py
+++ src/Q.py
@@ -2,29 +2,32 @@ import gym
 import random
 
 class Q:
+    def __init__(self, env):
+        self.env = env
+
     learningRate = 1
     discountFactor = 0.2
     qMap = {}
     def calculate(self, state):
         defaultQ = 1
-        result = [None]*env.action_space.n
+        result = [None]*self.env.action_space.n
         stateKey = str(state)
-        if stateKey not in qMap.keys():
-            qMap[stateKey] = dict()
-        for action in range(env.action_space.n):
-            if action not in qMap[stateKey].keys():
-                qMap[stateKey][action] = defaultQ
-            result[action] = qMap[stateKey][action]
+        if stateKey not in self.qMap.keys():
+            self.qMap[stateKey] = dict()
+        for action in range(self.env.action_space.n):
+            if action not in self.qMap[stateKey].keys():
+                self.qMap[stateKey][action] = defaultQ
+            result[action] = self.qMap[stateKey][action]
         return result
 
-    def learn(self,previousState, action, currentState, reward):
+    def learn(self, previousState, action, currentState, reward):
         oldValue = self.calculate(previousState)[action]
         result = oldValue + self.learningRate * (self.getLearnedValue(currentState,reward) - oldValue)
     def getLearnedValue(self, currentState, reward):
         return reward + self.discountFactor * max(self.calculate(currentState))
 
 class MainClass:
-    def getState(observation,env):
+    def getState(self, observation, env):
         DISCRETE = 5
         high = env.observation_space.high
         low = env.observation_space.low
@@ -40,7 +43,7 @@ class MainClass:
             result[i] = num
         return result
 
-    def chooseAction(qValues):
+    def chooseAction(self, qValues):
         sum = sum(qValues)
         randValue = random.random() * sum
         index = 0
@@ -49,21 +52,23 @@ class MainClass:
                 return index
             randValue -= value
             index += 1   
-      
 
-    def execute():    
+    def execute(self):
         env = gym.make('CartPole-v0')
+        q = Q(env)
     
         observation = env.reset()
         done = False
 
         while not done:
-            state = getState(observation,env)
-            qValues = calculateQs(state,env)
-            action = chooseAction(qValues)
+            state = self.getState(observation, env)
+            qValues = q.calculate(state, env)
+            action = self.chooseAction(qValues)
             previousState = state
             observation, reward, done, info = env.step(action)
-            learnQ(previousState, action, state, reward)
+            state = self.getState(observation, env)
+            q.learn(previousState, action, state, reward)
+            env.render()
 
 
 def main(args=None):
